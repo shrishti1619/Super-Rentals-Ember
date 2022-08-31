0269c2a070544abd317f2a63d42c7d6ee3c8806c
Shrishti <shrishtiojha1619@gmail.com>
2022-08-31 16:07:12 +0530

    Search Bar Added

--- app/routes/rentals.js
+++ app/routes/rentals.js
@@ -1,23 +1,12 @@
 import Route from '@ember/routing/route';
+import { service } from '@ember/service';
 
-const COMMUNITY_CATEGORIES = ['Condo', 'Townhouse', 'Apartment'];
 
 export default class IndexRoute extends Route {
+  @service store;
+
   async model(params) {
-    let response = await fetch(`/api/rentals/${params.rental_id}.json`);
-    let { data } = await response.json();
-
-    
-      let { id, attributes } = data;
-      let type;
-
-      if (COMMUNITY_CATEGORIES.includes(attributes.category)) {
-        type = 'Community';
-      } else {
-        type = 'Standalone';
-      }
-
-      return { id, type, ...attributes };
-    }
+    return this.store.findRecord('rental', params.rental_id);
   }
+}
 
